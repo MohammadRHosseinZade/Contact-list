9c52225f5995796b10be31370c5e886f70c484b6
mohammad <mohammadrhosseinzade@gmail.com>
2024-07-17 17:43:38 +0330

    added conacts list for a user endpoint

--- Contacts/urls.py
+++ Contacts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import GeneratePhoneContactView
+from .views import GeneratePhoneContactView, UserContactlistView
 
 
 urlpatterns = [
     path('generate/', GeneratePhoneContactView.as_view(), name='Generating Contact'),
-
+    path('retrieve_list/', UserContactlistView.as_view(), name='Generating Contact'),
 ]
--- Contacts/views.py
+++ Contacts/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status,parsers
+from django.forms.models import model_to_dict
 from drf_spectacular.utils import extend_schema
 from .serializers import GenerateContactSerializer
 from .models import (ContactDetail, UserContactsDetail, Phone2ContactDetail, PhoneNumber)
@@ -30,3 +31,22 @@ class GeneratePhoneContactView(APIView):
             except Exception as e:
                 return Response({'error': f'Error generating contact: {e}'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+    
+class UserContactlistView(APIView):
+    permission_classes = [IsAuthenticated]
+    @extend_schema(
+        description='Generating a Contact for a user',
+        responses={200: 'application/json'}
+    )
+    def get(self, request):
+            try:
+                user = self.request.user
+                obj_list = UserContactsDetail.objects.filter(user_id = user)
+                result = []
+                for obj in obj_list:
+                    result.append({"detail" : model_to_dict(obj.phone_detail_id.detail_id),
+                    "number" : obj.phone_detail_id.phone_id.phone_number})
+                return Response({'data': result}, status=status.HTTP_200_OK)
+            except Exception as e:
+                return Response({'error': f'Error generating contact: {e}'}, status=status.HTTP_400_BAD_REQUEST)   
+    
