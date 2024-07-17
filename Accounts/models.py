1ca7711440323c52cd9c697dc4bbe585374ee630
mohammad <mohammadrhosseinzade@gmail.com>
2024-07-17 13:14:35 +0330

    added costum user model

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name="email address", max_length=255, unique=True, db_index=True)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email 
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+    @is_staff.setter
+    def is_staff(self, value):
+        self.is_admin = value
+    
+    def has_module_perms(self, app_label):
+       return self.is_admin
+    
+    def has_perm(self, perm, obj=None):
+       return self.is_admin
+    
